2cf8de1e8015d1b2536dac597a685b6c1fd12dd1
Sthefany Campos <stmario.campos@icloud.com>
2019-05-15 19:47:27 -0600

    Actualizacion GitHub

---
+++ Proyectos Pilas,Colas y Listas/TorreHanoi/src/torrehanoi/Nodo.java
@@ -0,0 +1,12 @@
+package torrehanoi;
+/**
+ * Sthefany Johana Campos Hernández
+ * 0901-17-2452
+ */
+public class Nodo {
+    int info;
+    public Nodo Siguiente;
+    public Nodo (int dato){
+	info=dato;
+        }
+}
